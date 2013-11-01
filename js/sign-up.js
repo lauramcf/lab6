f0ef2c96bf35ddbb37713f1cfd0555d792f1ca12
unknown <iguest@is-m430m27-25.ischool.uw.edu>
2013-11-01 09:19:20 -0700

    lab6

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,34 @@
 //signup.js 
 // add your JavaScript code to this file
 
+$(function(){
+	var option;
+    var idx;
+    var state;
+    var stateSelect = $('#state');
+    for(idx = 0; idx < usStates.length; ++idx) {
+            state = usStates[idx];
+            option = $(document.createElement('option'));
+            option.attr('value', state.abbreviation);
+            option.html(state.name);
+            stateSelect.append(option);
+    }
+});
+
+$('.signup-form').submit(function(){
+    //code to execute when the sign-up form is submitted
+    //this is the raw DOM form element
+    //wrap it in a jQuery object so we can use jQuery methods on it
+    var signupForm = $(this);	
+    var addr1Input = signupForm.find('input[name="addr-1"]');
+    var addr1Value = addr1Input.val();
+    if(null != addr1Value && addr1Value.length > 0){
+	    var zipInput = signupForm.find('input[name="zip"]');
+	    var zipValue = zipInput.val();
+	    var valid = (null != zipValue && zipValue.length > 0);
+	    if(!valid) {
+	    	alert("Please input your zip code");
+	    }
+	  return valid;  
+	}
+});
